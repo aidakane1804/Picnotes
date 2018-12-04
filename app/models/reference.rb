044f97c57154addde7f399de7132f57f5f0d5043
cowfun <cowfun87@gmail.com>
2018-12-03 16:43:39 -0800

    fixed tags delimiter

--- app/models/reference.rb
+++ app/models/reference.rb
@@ -1,4 +1,12 @@
 class Reference < ApplicationRecord
   belongs_to :note
   validates :title, presence: :true
+
+  before_validation :smart_add_url_protocol
+
+  def smart_add_url_protocol
+    unless self.link[/\Ahttp:\/\//] || self.link[/\Ahttps:\/\//]
+      self.link = "http://#{self.link}"
+    end
+  end
 end
