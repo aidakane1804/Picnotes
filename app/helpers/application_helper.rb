b01b5c273aafda4ca816f4f7a07a912c0628f7d4
cowfun <cowfun87@gmail.com>
2019-01-18 11:08:54 -0800

    fixing meta

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,16 +2,4 @@ module ApplicationHelper
   def cp(path)
     'current' if current_page?(path)
   end
-
-#   def og_image_path
-#   if @note.present?
-#     @note.image.url
-#   end
-# end
-#
-# def og_url_path
-#   if @note.present?
-#     # "http://www.picnotes.org/notes/" + @note.id.to_param
-#   end
-# end
 end
