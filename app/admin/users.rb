3287b0651c6ba210036fcc6da9977898149fdba1
cowfun <cowfun87@gmail.com>
2019-02-08 12:54:00 -0800

    allows admin to view users - working on changing password

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,4 +1,4 @@
-ActiveAdmin.register User do
+ActiveAdmin.register User, as: 'People' do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
@@ -11,5 +11,6 @@ ActiveAdmin.register User do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+
 
 end
