d0f521c1a7cb7af3dae8aaca2d19e44c54bee21b
cowfun <cowfun87@gmail.com>
2019-02-12 17:51:38 -0800

    reset password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -5,7 +5,7 @@ class PasswordResetsController < ApplicationController
   def create
     @user = User.find_by(email: params[:password_reset][:email].downcase)
     if @user
-      @user.send_password_reset_email
+      UserMailer.password_reset(@user).deliver_now
       flash[:info] = "Email sent with password reset instructions"
       redirect_to root_url
     else
