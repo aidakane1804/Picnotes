79d1211b2999a3035237a576e796bdd7312b02cc
AteqEjaz <ateq.odesk@gmail.com>
2019-03-15 02:42:20 +0500

    added amazon links in books

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def cp(path)
     'current' if current_page?(path)
   end
+
+  def strip_url(target_url)
+  target_url.gsub("http://", "")
+            .gsub("https://", "")
+            .gsub("www.", "")
+end
 end
