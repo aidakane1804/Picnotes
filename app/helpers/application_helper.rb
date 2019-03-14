8cc986c2413425aaa14d5747eea8721e853b63cc
AteqEjaz <ateq.odesk@gmail.com>
2019-03-15 04:25:00 +0500

    fixed minor issue in url.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,4 @@ module ApplicationHelper
   def cp(path)
     'current' if current_page?(path)
   end
-
-  def strip_url(target_url)
-  target_url.gsub("http://", "")
-            .gsub("https://", "")
-            .gsub("www.", "")
-end
 end
