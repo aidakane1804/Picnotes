02de326f3d6f321b590af31fbf51e038e24a43bb
Ateq Ejaz <ateq.odesk@gmail.com>
2019-12-30 16:50:20 +0500

    fix issue

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -10,7 +10,7 @@ class FeedController < ApplicationController
     else
       @user = current_user
       @following = current_user.following rescue ''
-      @notes = Note.where(user_id: current_user.following.pluck(:id))
+      @notes = Note.where(user_id: current_user.following.pluck(:id)) rescue ''
       @notes = @notes.order(created_at: :desc)
     end
 
