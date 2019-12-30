c8f24dc15efcfedd197fb5c22b1ebf083b7e2834
Ateq Ejaz <ateq.odesk@gmail.com>
2019-12-30 16:52:51 +0500

    fix issue

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -4,14 +4,14 @@ class FeedController < ApplicationController
       # if params[:user_id] != current_user.id.to_s
         user = User.find_by(id: params[:user_id])
         @user = user
-        @following = user.following
-        @notes = Note.where(user_id: user.following.pluck(:id))
-        @notes = @notes.order(created_at: :desc)
+        @following = user.following rescue ''
+        @notes = Note.where(user_id: user.following.pluck(:id)) rescue ''
+        @notes = @notes.order(created_at: :desc) rescue ''
     else
       @user = current_user
       @following = current_user.following rescue ''
       @notes = Note.where(user_id: current_user.following.pluck(:id)) rescue ''
-      @notes = @notes.order(created_at: :desc)
+      @notes = @notes.order(created_at: :desc) rescue ''
     end
 
   end
