c2330faae5c851bccee3e8ad33f94bb1814d7ef2
Ateq Ejaz <ateq.odesk@gmail.com>
2019-12-30 16:07:46 +0500

    fix Can't verify CSRF token authenticity

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
   before_action :initiate_instance_variables
   include UsersHelper
   def user_signed_in?
