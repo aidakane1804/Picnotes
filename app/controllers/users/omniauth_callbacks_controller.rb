7026a0b9146402e516b94b970a2094297ba8946e
Ateq Ejaz <ateq.odesk@gmail.com>
2020-07-14 22:07:41 +0500

    old app issue fixed

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,6 +5,16 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, :event => :authentication
       set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
       session[:user_id] = User.find_by_uid(@user.uid).id
+    elsif  !(@user.persisted?)
+      @user = User.find_by_email(request.env['omniauth.auth'].info.email)
+      if @user.persisted?
+        @user.provider= request.env['omniauth.auth'].provider
+        @user.uid=request.env['omniauth.auth'].uid
+        @user.save
+        sign_in_and_redirect @user, :event => :authentication
+        set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
+        session[:user_id] = User.find_by_uid(@user.uid).id
+      end
     else
       Rails.logger.info "###################{@user.errors.messages.inspect}@@@@@@@@@@@@@@@@@@@@"
       flash[:error] = 'There was a problem signing you in through Facebook. Please register or try signing in later.'
