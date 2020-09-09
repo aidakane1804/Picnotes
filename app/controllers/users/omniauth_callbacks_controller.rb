a90d35b5b29b971b1b10a6bd0fd754481beb8625
AnkurBajad <ankurbajad786@gmail.com>
2020-09-09 22:18:36 +0530

    Fixed# google signin and signup

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -24,4 +24,18 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def failure
     redirect_to root_path
   end
+
+  def google_oauth2
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+
+    if @user.persisted?
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+        sign_in_and_redirect @user, :event => :authentication
+        session[:user_id] = @user.id
+    else
+      session['devise.google_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
+      redirect_to new_user_board_url, alert: @user.errors.full_messages.join("\n")
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   acts_as_voter
 
-  devise :omniauthable, :omniauth_providers => [:facebook]
+  devise :omniauthable, :omniauth_providers => [:facebook, :google_oauth2]
   # devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook]
 
   has_many :folders, dependent: :destroy
@@ -94,6 +94,23 @@ class User < ApplicationRecord
       end
     end
   end
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+    #Uncomment the section below if you want users to be created if they don't exist
+    unless user
+        user = User.create(name: data['name'],
+          email: data['email'],
+          username: data['email'],
+          first_name: data['first_name'],
+          last_name: data['last_name'],
+          password: Devise.friendly_token[0,20]
+        )
+    end
+    user
+  end
+
 
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
