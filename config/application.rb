527990eca023e8df701bd8ffb5882eb2c369542a
AnkurBajad <ankurbajad786@gmail.com>
2021-01-14 18:35:28 +0530

    Add some changes

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,8 @@ module PicnotesServer
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.assets.enabled = true
+    config.serve_static_assets = true
 
   end
 end
