1198978815905b91190eadee8a1a1f5a9d634d47
Ateq <Ateq.odesk@gmail.com>
2021-05-18 15:27:05 +0500

    debugging

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.create_from_provider_data(request.env['omniauth.auth'])
+    Rails.logger.info "##########--------#{request.env['omniauth.auth'].inspect}--------"
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication
       set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
