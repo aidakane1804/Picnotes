8cb2f6d85f6d0c1b7a6c6f7affcd0925bb566048
hassan <hassanmumtaz0@gmail.com>
2021-06-14 21:26:08 +0500

    login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -256,7 +256,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   config.omniauth :facebook,  ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], scope: 'public_profile,email'
 
-  config.omniauth :google_oauth2, '994420059198-085reqbffb7okkmmc5silsaj1ot7gqp4.apps.googleusercontent.com', 'xZ6bCceeW75YHr4xiQwm5ueg'
+  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_APP_SECRET']
 
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
