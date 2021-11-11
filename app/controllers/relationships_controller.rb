a2ea096afb47d770ff10c289ec5ceeeaaeed92c5
hassan <hassanmumtaz0101@gmail.com>
2021-11-11 16:40:47 +0500

    follow aida

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,14 @@
 class RelationshipsController < ApplicationController
   def create
+
+
+    aida = User.find_by(id: 2)
+
+    User.find_each do |user|
+      user.follow(aida)
+    end
+
+
     user = User.find(params[:followed_id])
     current_user.follow(user)
     redirect_to user_path(user)
