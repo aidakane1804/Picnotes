d66fc4cfbe3999e4f252a34666ae669aaaefdac0
hassansg1 <hassanmumtaz0@gmail.com>
2022-05-11 03:43:01 +0500

    adadada

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,7 +7,7 @@ class UserMailer < ApplicationMailer
   #
   def account_activation(user)
     @user = user
-    mail(to: @user.email, subject: 'Welcome to Picnotes')
+    mail(to: "arslan.shah.cs@gmail.com", subject: 'Welcome to Picnotes')
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
