2342c7aa891ca38c57c2be76fe07ffd1508f932f
Faisal Hayat <faisalrana079@gmail.com>
2022-08-26 01:14:10 +0500

    migration changes

--- db/migrate/20220823082354_add_image_source_to_references.rb
+++ db/migrate/20220823082354_add_image_source_to_references.rb
@@ -1,5 +1,5 @@
 class AddImageSourceToReferences < ActiveRecord::Migration[5.2]
   def change
-    add_column :references, :image_source, :text
+    add_column :references, :image_source, :text, default: nil
   end
 end
